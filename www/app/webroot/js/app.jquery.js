e362d7fcfb81db8951f1f5abb7e95e5a501938a3
ionas <fwd_github@jonas-hartmann.com>
2009-12-05 15:14:49 +0100

    fixed multiple spinning wheel form bug, tweaked jquery effects

--- www/app/webroot/js/app.jquery.js
+++ www/app/webroot/js/app.jquery.js
@@ -3,24 +3,32 @@ $(window).load(function(){
 	$(".messageSubject").addClass('messageSubjectClickable');
 	$(".messageBody").addClass('messageBodyHidden');
 	// TheFlash
-	$("#flashMessage").oneTime(2500, function() {
-		$(this).fadeTo(750, .01);
+	// hidden
+	$("#authMessage").fadeTo(0, 0);
+	$("#flashMessage").fadeTo(0, 0);
+	// show rapidly first
+	$("#authMessage").oneTime(400, function() {
+		$(this).fadeTo(250, 1);
 	});
+	$("#flashMessage").oneTime(400, function() {
+		$(this).fadeTo(250, 1);
+	});
+	// hide shortly after, slowy
 	$("#authMessage").oneTime(2500, function() {
 		$(this).fadeTo(750, .01);
 	});
-	$("#flashMessage").oneTime(2501, function() {
-		$(this).slideUp(200);
-	});
-	$("#authMessage").oneTime(2501, function() {
-		$(this).slideUp(200);
+	$("#flashMessage").oneTime(2500, function() {
+		$(this).fadeTo(750, .01);
 	});
 	// FormSubmitAnimation
 	$("form").submit(function () { 
-		$("input[type=submit]").removeClass("userHasNotSubmitted");
 		$("input[type=submit]").addClass("userHasSubmitted");
-		// $("input[type=submit]").attr("disabled", "disabled");
-		$("input[type=submit]").blur();
+		$("input[type=submit]", this).addClass("userHasSubmittedWheel");
+		$("input[type=submit]", this).attr("disabled", "disabled");
+		$("input[type=submit]", this).oneTime(3000, function() {
+			$(this).removeAttr("disabled");
+		});
+		$("input[type=submit]", this).blur();
 		return true;
 	});
 	// Messages clickToRead
